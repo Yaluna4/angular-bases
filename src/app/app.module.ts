cedd0d599ab9deaee597ffecdb4c5da5cd7a5edd
Yamisbel Orlianys Luna Sojo <ysojo@smsargentina.com.ar>
2024-10-23 14:49:52 -0300

    primera app de angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CounterComponent } from './counter/counter.component';
+import { HeroComponent } from './counter/heroes/hero/hero.component';
+import { ListComponent } from './counter/heroes/list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterComponent,
+    HeroComponent,
+    ListComponent,
   ],
   imports: [
     BrowserModule,
